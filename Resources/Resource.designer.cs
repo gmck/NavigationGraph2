258efb4612774f942539bcc89532505d90a1a8ca
gmckechnie <gmckechnie@glmsoftware.com>
2022-07-01 17:44:46 +1000

    Added NavigationGraph.docx

--- Resources/Resource.designer.cs
+++ Resources/Resource.designer.cs
@@ -14,7 +14,7 @@ namespace com.companyname.NavigationGraph2
 {
 	
 	
-	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "12.3.0.26")]
+	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "12.3.99.16")]
 	public partial class Resource
 	{
 		
